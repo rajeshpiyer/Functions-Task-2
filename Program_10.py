0d3d6b74bf8bb5d4170cc6c96591cec047c80540
Rajesh P Iyer <90857981+rajeshpiyer@users.noreply.github.com>
2024-02-13 17:13:54 +0530

    Add files via upload

---
+++ Program_10.py
@@ -0,0 +1,6 @@
+from rectangle import area
+
+length = int(input("Enter the length of rectangle : "))
+breadth = int(input("Enter the breadth of rectangle : "))
+result = area(length,breadth)
+print("Area : ",result)
